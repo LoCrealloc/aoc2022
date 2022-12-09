6c52a0f53d0b1fd6580041ca97d41a78874bfc42
LoC <loc@locrealloc.de>
2022-12-09 20:28:10 +0100

    Added a solution for day 9 part 2
    Signed-off-by: LoC <loc@locrealloc.de>

--- solutions/day09.py
+++ solutions/day09.py
@@ -43,5 +43,46 @@ def part_1():
 
     return len(visited)
 
+
+def part_2():
+    visited = set()
+    rope = [(0, 0)] * 9
+
+    xH = 0
+    yH = 0
+
+    for line in puzzle:
+        d, t = line.split()
+
+        for i in range(int(t)):
+            if d in ("L", "R"):
+                xH = xH + 1 if d == "R" else xH - 1
+
+            else:
+                yH = yH + 1 if d == "U" else yH - 1
+
+            for index, (x, y) in enumerate(rope):
+                formerKnot = rope[index - 1] if index > 0 else (xH, yH)
+
+                calculated = calcAllowed(*formerKnot)
+
+                if (x, y) not in calculated:
+
+                    straights = ((x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y))
+                    diagonals = ((x + 1, y + 1), (x + 1, y - 1), (x - 1, y - 1), (x - 1, y + 1))
+
+                    tries = straights if formerKnot[0] == x or formerKnot[1] == y else diagonals
+
+                    for tr in tries:
+                        if tr in calculated:
+                            rope[index] = tr
+                            break
+
+            visited.add(rope[-1])
+
+    return len(visited)
+
+
 if __name__ == '__main__':
     print(part_1())
+    print(part_2())
